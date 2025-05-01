23535aa18e58c26ea411ecb127f109ffdedf371a
thechosea <thechosea@gmail.com>
2025-05-01 08:24:13 +0000

    06 id2 edit

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             กูมาแล้วไอ่เวร
           </li>
+          <li className="tracking-[-.01em]">
+            id2 edit
+          </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
